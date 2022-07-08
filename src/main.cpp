cb6dffa92dcc2cf913c505328e9185527a8104a2
OnlyblackTea <38585636+OnlyblackTea@users.noreply.github.com>
2022-07-08 15:16:11 +0800

    One Try, Hello World

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <vlcpp/vlc.hpp>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "usage: " << argv[0] << " <file to play> " << std::endl;
+        return 1;
+    }
+
+    auto instance = VLC::Instance(0, nullptr);
+#if LIBVLC_VVERSION_INT >= LIBVLC_VERSION(4, 0, 0, 0)
+    auto media = VLC::Media(argv[1], VLC::Media::FromPath);
+    auto mp = VLC::MediaPlayer(instance, media);
+#else
+    auto media = VLC::Media(instance, argv[1], VLC::Media::FromPath);
+    auto mp = VLC::MediaPlayer(media);
+#endif
+    mp.play();
+    std::this_thread:sleep_for( std::chrono::seconds(10) );
+#if LIBVLC_VERSION_INT >= LIBVLC_VERSION(4, 0, 0, 0)
+    mp.stopAsync();
+#else
+    mp.stop();
+#endif
+}
